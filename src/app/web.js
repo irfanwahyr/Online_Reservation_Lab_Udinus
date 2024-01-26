d25b6dd3e15d4880030ce32de3c561a31c9012a6
irfan <iramadhani679@gmail.com>
2024-01-26 22:47:36 +0700

    add register user

--- src/app/web.js
+++ src/app/web.js
@@ -1,4 +1,10 @@
 import express from "express";
+import { publicRouter } from "../route/public_api";
+import { errorMiddleware } from "../middleware/error_middleware";
 
-export const web = express()
-web.use(express.json())
+export const web = express();
+web.use(express.json());
+
+
+web.use(publicRouter);
+web.use(errorMiddleware);
