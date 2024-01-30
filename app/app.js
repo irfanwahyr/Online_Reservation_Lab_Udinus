d33fbe6483824a4418505044426c9f923b51668f
irfan <iramadhani679@gmail.com>
2024-01-30 10:10:22 +0700

    add cors

--- app/app.js
+++ app/app.js
@@ -3,7 +3,16 @@ const app = express();
 const userRoute = require('../routes/users');
 const labRoute = require('../routes/labs');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const corsOptions = {
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,  // Mengizinkan pengiriman kredensial (cookie, token, dsb.)
+    optionsSuccessStatus: 204,
+  };
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use('/users', userRoute);
