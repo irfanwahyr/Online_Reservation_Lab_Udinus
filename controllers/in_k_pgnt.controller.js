627f9824bcf36de6fe611b1b74d5027de281ce5f
irfan <iramadhani679@gmail.com>
2024-02-07 15:12:02 +0700

    create in_k_pgnt API

---
+++ controllers/in_k_pgnt.controller.js
@@ -0,0 +1,159 @@
+const models = require('../models');
+const { Sequelize } = require('sequelize');
+
+async function create(req, res) {
+    try {
+        const { id_software ,nama, jml_PC, jenis_lab, deskripsi } = req.body;
+
+        // Data input valid, continue with the create operation
+        await models.Laboratorium.create({
+            id_software: id_software,
+            nama: nama,
+            jml_PC: jml_PC,
+            jenis_lab: jenis_lab,
+            deskripsi: deskripsi
+        });
+
+        return res.status(201).json({
+            message: "Created new Laboratorium"
+          });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong",
+        });
+    }
+}
+
+async function index(_, res) {
+    try {
+        const results = await models.Laboratorium.findAll({
+            include: [{
+                model: models.Software_Primer,
+                as: 'software_primers',
+                attributes: ['nama', 'versi']
+            },
+            {
+                model: models.Software_Sekunder,
+                as: 'software_sekunders',
+                attributes: ['nama', 'versi']
+            },
+        ]
+        });
+
+        if (results && results.length > 0) {
+            const labsData = results.map(({ id, nama, jml_PC, jenis_lab, deskripsi, software_primers ,software_sekunders }) => ({
+                id,
+                nama,
+                jml_PC,
+                jenis_lab,
+                deskripsi,
+                software_primers,
+                software_sekunders,
+            }));
+
+            res.status(200).json(labsData);
+        } else {
+            res.status(200).json({
+                message: "No Labs found",
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Unable to retrieve labs data. Something went wrong.",
+        });
+    }
+}
+
+
+async function update(req, res) {
+    try {
+      const id = req.params.id;
+      const { id_software, nama, jml_PC, jenis_lab, deskripsi } = req.body;
+
+      const [updatedRowsCount] = await models.Laboratorium.update(
+        { id_software ,nama, jml_PC, jenis_lab, deskripsi },
+        { where: { id: id } }
+      );
+
+      if (updatedRowsCount > 0) {
+        res.status(200).json({
+          message: "Labs updated successfully",
+        });
+      } else {
+        res.status(404).json({
+          status: res.status,
+          message: "Labs not found",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        status: res.status,
+        message: "Internal Server Error",
+      });
+    }
+  }
+
+async function destroy(req, res) {
+    try {
+        const id = req.params.id;
+
+        const deletedRowsCount = await models.Laboratorium.destroy({
+            where: { id: id }
+        });
+
+        if (deletedRowsCount > 0) {
+            res.status(200).json({
+                status: res.status,
+                message: "labs deleted successfully",
+            });
+        } else {
+            res.status(404).json({
+                status: res.status,
+                message: "labs not found",
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: res.status,
+            message: "Internal Server Error",
+        });
+    }
+}
+
+async function show_by_id(req, res) {
+    try {
+        const id = req.params.id;
+
+        const result = await models.Laboratorium.findByPk(id);
+
+        if (result) {
+            const {id_software, nama, jml_PC, jenis_lab, deskripsi } = result;
+            res.status(200).json({
+                id_software,
+                nama,
+                jml_PC,
+                jenis_lab,
+                deskripsi
+            });
+        } else {
+            res.status(404).json({
+                message: "Labs not found",
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+}
+
+module.exports = {
+    create,
+    index,
+    update,
+    destroy,
+    show_by_id
+  };
