850b5519ddeab1ad09d12426c234a98cb999973b
irfan <iramadhani679@gmail.com>
2024-04-02 11:02:17 +0700

    add new rule pada file-uploader.js dan convertnya

--- helpers/file-uploader.js
+++ helpers/file-uploader.js
@@ -1,5 +1,7 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const { exec } = require('child_process');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -10,31 +12,52 @@ const storage = multer.diskStorage({
     }
 });
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'application/pdf') {
-        cb(null, true);
-    } else {
-        cb(new Error('Unsupported file type'), false);
-    }
-}
-
 const upload = multer({
     storage: storage,
     limits: {
         fileSize: 1024*1024*2
     },
-    // fileFilter: fileFilter
 });
 
-const multiUpload = upload.fields(
-    [
+const multiUpload = (req, res, next) => {
+    upload.fields([
         {name: 'proposal_acara'},
         {name: 'surat_peminjaman'},
+    ])(req, res, (err) => {
+        if (err) {
+            return next(err);
+        }
 
-    ]
-)
+        if (req.files['proposal_acara']) {
+            const filePath = req.files['proposal_acara'][0].path;
+            convertToPDF(filePath);
+        }
 
+        if (req.files['surat_peminjaman']) {
+            const filePath = req.files['surat_peminjaman'][0].path;
+            convertToPDF(filePath);
+        }
+
+        next();
+    });
+};
+
+const convertToPDF = (filePath) => {
+    const pdfPath = filePath.replace(path.extname(filePath), '.pdf');
+
+    exec(`convert ${filePath} ${pdfPath}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error converting file to PDF: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Error converting file to PDF: ${stderr}`);
+            return;
+        }
+        console.log(`File converted to PDF: ${pdfPath}`);
+    });
+};
 
 module.exports = {
     multiUpload,
-}
+};
